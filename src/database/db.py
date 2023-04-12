35ed83e577e2562a19cb5bc64bf767379f36335e
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2023-04-12 16:25:31 +0300

    create documentation

--- src/database/db.py
+++ src/database/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from src.conf.config import settings
 
 
-db_url = "postgresql://gkofooxx:cqeFSr15Em1MIi-2cJVme9GfsdBZzh5b@snuffleupagus.db.elephantsql.com/gkofooxx"
+db_url = settings.sqlalchemy_database_url
 engine = create_engine(db_url)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -15,7 +15,7 @@ def get_db():
     The get_db function is a context manager that returns the database session.
     It also ensures that the connection to the database is closed after each request.
 
-    :return: A database sessionlocal object
+    :return: A database session
     :doc-author: Trelent
     """
     db = SessionLocal()
